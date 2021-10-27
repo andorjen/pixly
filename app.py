1c9bc595788109666629f8ae3753b554bef38871
Jenny Liu <jennyliuye@gmail.com>
2021-10-27 15:09:29 -0700

    fix s3 uploading bug

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from flask_debugtoolbar import DebugToolbarExtension
 from flask import Flask, render_template, redirect, request
 from models import db, connect_db, Image
 # from forms import FormName
+# from werkzeug.utils import secure_filename
 
 from PIL import Image as PILImage
 from PIL.ExifTags import TAGS
@@ -23,7 +24,7 @@ app.config['SQLALCHEMY_ECHO'] = True
 # comes from .env file, add to .gitignore
 MY_SECRET = os.environ['MY_SECRET']
 API_KEY = os.environ['API_SECRET_KEY']
-
+# AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 
 app.config['SECRET_KEY'] = MY_SECRET
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
@@ -33,11 +34,12 @@ connect_db(app)
 db.create_all()
 
 
-s3 = boto3.resource('s3')
-# AWS_BUCKET_URL = "https://pix.ly-eaa.s3.us-west-2.amazonaws.com/"
-AWS_OBJECT_URL = "https://s3.us-west-2.amazonaws.com/pix.ly-eaa/"
+# s3 = boto3.resource('s3')
+s3 = boto3.client('s3')
 
-# AWS_BUCKET_URL = "https://pixly-alien-j.s3.us-west-1.amazonaws.com/"
+# AWS_OBJECT_URL = "https://s3.us-west-2.amazonaws.com/pix.ly-eaa/"
+
+AWS_OBJECT_URL = "https://pixly-alien-j.s3.us-west-1.amazonaws.com/"
 
 
 @app.get("/")
@@ -59,21 +61,40 @@ def add_image():
     """upload template"""
 
     user_title = request.form['title']
-    print("From /upload, image:", request.files['image'])
-    breakpoint()
+    file = request.files['image']
+
+    # data = file.read()
+
+    # breakpoint()
     image_title = ("").join(user_title.split())
     image_id = uuid.uuid4()
-
-    image_data = get_image_data(request.files['image'])
+    image_data = get_image_data(file)
 
     print(image_data)
 
-    s3.meta.client.upload_fileobj(
-        request.files['image'], "pix.ly-eaa", f"{image_title}-{image_id}", ExtraArgs={"ContentType": "image/jpeg"})
+    # s3.meta.client.upload_fileobj(
+    #     file, "pix.ly-eaa", f"{image_title}-{image_id}", ExtraArgs={"ContentType": "image/jpeg"})
+    # breakpoint()
+    file.seek(0)
+    result = s3.upload_fileobj(
+        file, "pixly-alien-j", f"{image_title}-{image_id}",
+        ExtraArgs={"ACL": "public-read"})
+
+    #  secure_filename(file.filename)
+    # result = s3.upload_file(
+    #     secure_filename(
+    #         file.filename), "pixly-alien-j", f"{image_title}-{image_id}",
+    #     ExtraArgs={"ContentType": file.content_type, "ACL": "public-read"})
+
+    print("after upload", result)
+
+    # ExtraArgs={"Metadata": {"ContentType": "image/jpeg"}}
+    # s3.meta.client.upload_fileobj(
+    #     file, "pixly-alien-j", f"{image_title}-{image_id}", ExtraArgs={"ContentType": "image/jpeg"})
     # s3.Bucket(
-    #     'pix.ly-eaa').put_object(Key=f"{image_title}-{image_id}", Body=request.files['image'], ContentType="image/jpeg")
+    #     'pix.ly-eaa').put_object(Key=f"{image_title}-{image_id}", Body=file, ContentType="image/jpeg")
     # s3.Bucket(
-    #     'pixly-alien-j').put_object(Key=f"{image_title}-{image_id}", Body=request.files['image'])
+    #     # 'pixly-alien-j').put_object(Key=f"{image_title}-{image_id}", Body=file)
 
     image = Image(
         id=image_id,
@@ -96,18 +117,13 @@ def show_all_images():
     return render_template("images.html", images=images)
 
 
-
-
-
-
-
 def get_image_data(path):
     """read metadata from a given image"""
-    # breakpoint()
+
     image = PILImage.open(path)
     exifdata = image.getexif()
     result = {}
-    # breakpoint()
+
     for tag_id in exifdata:
         # print("in the tag_id decode")
         # get the tag name, instead of human unreadable tag id
@@ -120,3 +136,4 @@ def get_image_data(path):
         result[tag] = data
 
     return result
+# pillow image open context manager : with
